71ac1d754d536322faecc515f8edfb36550c2288
marakosh <marat.garifulin160@gmail.com>
2022-10-25 23:29:13 +0400

    12.08

---
+++ Fraction/Fraction/Fraction.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction_
+{
+    internal class Fraction
+    {
+        public int denominator { get; }
+        public int numerator { get; }
+        public Fraction(Int32 dividend, Int32 divider)
+        {
+            this.denominator = dividend * (divider >= 0 ? 1 : -1);
+            this.numerator = divider * (divider >= 0 ? 1 : -1);
+        }
+
+        public static Fraction operator +(Fraction A, Fraction B)
+        {
+            if (A.numerator == B.numerator) 
+                return new Fraction(A.denominator + B.denominator, A.numerator);
+
+            else if (A.numerator % B.numerator == 0 || B.numerator % A.numerator == 0)
+            {
+                return new Fraction(A.denominator * (B.numerator / A.numerator == 0 ? 1 : B.numerator / A.numerator) +
+                                   B.denominator * (A.numerator / B.numerator == 0 ? 1 : A.numerator / B.numerator),
+                                   (A.numerator > B.numerator ? A.numerator : B.numerator));
+            }
+            return new Fraction(A.denominator * B.numerator + B.denominator * A.numerator,
+                A.numerator * B.numerator);
+        }
+        public static Fraction operator -(Fraction A, Fraction B)
+        {
+            if (A.numerator == B.numerator) 
+                return new Fraction(A.denominator - B.denominator, A.numerator);
+            else if (A.numerator % B.numerator == 0 || B.numerator % A.numerator == 0)
+            {
+                return new Fraction(A.denominator * (B.numerator / A.numerator == 0 ? 1 : B.numerator / A.numerator) -
+                                   B.denominator * (A.numerator / B.numerator == 0 ? 1 : A.numerator / B.numerator),
+                                   (A.numerator > B.numerator ? A.numerator : B.numerator));
+            }
+            return new Fraction(A.denominator * B.denominator - B.denominator * A.denominator,
+                A.denominator * B.denominator);
+        }
+        public static Fraction operator *(Fraction A, Fraction B) => 
+            new Fraction(A.denominator * B.denominator, A.numerator * B.numerator);
+        public static Fraction operator /(Fraction A, Fraction B) =>
+            new Fraction(A.denominator * B.numerator, A.numerator * B.denominator);
+        public static bool operator >(Fraction A, Fraction B) =>
+            ((Single)A.denominator / (Single)A.numerator > B.denominator / (Single)B.numerator);
+        public static bool operator <(Fraction A, Fraction B) => 
+            ((Single)A.denominator / (Single)A.numerator < B.denominator / (Single)B.numerator);
+        public static bool operator ==(Fraction A, Fraction B) => 
+            ((Single)A.denominator / (Single)A.numerator == B.denominator / (Single)B.numerator);
+        public static bool operator !=(Fraction A, Fraction B) => 
+            ((Single)A.denominator / (Single)A.numerator != B.denominator / (Single)B.numerator);
+
+
+
+        public override bool Equals(object? obj) => obj is Fraction Fraction &&
+                  (Single)this.denominator / (Single)this.numerator == (Single)Fraction.denominator / (Single)Fraction.numerator;
+        public override string ToString() 
+            => $"({this.denominator / this.numerator}) {this.denominator % this.numerator}/{this.numerator}";
+    }
+}
---
+++ Fraction/Fraction/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Fraction_
+{
+    class Program
+    {
+        public static Fraction solution_fraction(char solution, ref Fraction A, Fraction B) => solution switch
+        {
+            '*' => A *= B,
+            '/' => A /= B,
+            '+' => A += B,
+            _ => A -= B
+        };
+
+        public static void Main()
+        {
+            Console.Write("enter dividend 1 : ");
+            Int32 denominator1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter divider 1 : ");
+            Int32 numerator1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter dividend 2 : ");
+            Int32 denominator2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter divider 2 : ");
+            Int32 numerator2 = Convert.ToInt32(Console.ReadLine());
+            Fraction A = new Fraction(denominator1, numerator1);
+            Fraction B = new Fraction(denominator2, numerator2);
+            Console.Write("enter ( * / + - ) : ");
+            Char select = Convert.ToChar(Console.Read());
+            solution_fraction(solution: select, A: ref A, B: B);
+            Console.WriteLine(A);
+        }
+    }
+}
+
